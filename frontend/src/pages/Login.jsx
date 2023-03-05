7f23052cc2206e54fd2801bf222e31dccb2315a0
abdulsagheer <abdulsagheeras29@gmail.com>
2023-03-06 00:00:43 +0530

    Implemented Login functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import "../styles/RegiserStyles.css";
 import { Form, Input, message } from "antd";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Login = () => {
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
 	//form handler
 	const onfinishHandler = async (values) => {
 		try {
+			dispatch(showLoading());
+
 			const res = await axios.post(
 				`${import.meta.env.VITE_API_URL}/api/v1/user/login`,
 				values
 			);
+			window.location.reload();
+			dispatch(hideLoading());
 			if (res.data.success) {
 				localStorage.setItem("token", res.data.token);
 				message.success("Login Successfully");
@@ -21,12 +29,13 @@ const Login = () => {
 				message.error(res.data.message);
 			}
 		} catch (error) {
+			dispatch(hideLoading());
 			console.log(error);
 			message.error("something went wrong");
 		}
 	};
 	return (
-		<div className="form-container">
+		<div className="form-container ">
 			<Form
 				layout="vertical"
 				onFinish={onfinishHandler}
@@ -42,7 +51,7 @@ const Login = () => {
 				<Link to="/register" className="m-2">
 					Not a user Register here
 				</Link>
-				<div className="mt-4">
+				<div className="text-center mt-4">
 					<button className="btn btn-primary" type="submit">
 						Login
 					</button>
