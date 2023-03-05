52b7fb3c7c95595cfb2d22ac811a774159c8d752
abdulsagheer <abdulsagheeras29@gmail.com>
2023-03-06 00:00:34 +0530

    Added Home page functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Layout from "../components/Layout";
+import { Row } from "antd";
+import DoctorList from "../components/DoctorList";
 
 const Home = () => {
-	return <div>Home</div>;
+	const [doctors, setDoctors] = useState([]);
+	// login user data
+	const getUserData = async () => {
+		try {
+			const res = await axios.get(
+				`${import.meta.env.VITE_API_URL}/api/v1/user/getAllDoctors`,
+				{
+					headers: {
+						Authorization: "Bearer " + localStorage.getItem("token"),
+					},
+				}
+			);
+			if (res.data.success) {
+				setDoctors(res.data.data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getUserData();
+	}, []);
+
+	return (
+		<Layout>
+			<h1 className="text-center">Home</h1>
+			<Row>
+				{doctors &&
+					doctors.map((doctor) => (
+						<DoctorList key={doctor._id} doctor={doctor} />
+					))}
+			</Row>
+		</Layout>
+	);
 };
 
 export default Home;
