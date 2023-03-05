0704c2f00bfe80014646b1897284af9e85731e91
abdulsagheer <abdulsagheeras29@gmail.com>
2023-03-05 14:26:37 +0530

    Implemented Login functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import "../styles/RegiserStyles.css";
+import { Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-	return <div>Login</div>;
+	const navigate = useNavigate();
+	//form handler
+	const onfinishHandler = async (values) => {
+		try {
+			const res = await axios.post(
+				`${import.meta.env.VITE_API_URL}/api/v1/user/login`,
+				values
+			);
+			if (res.data.success) {
+				localStorage.setItem("token", res.data.token);
+				message.success("Login Successfully");
+				navigate("/");
+			} else {
+				message.error(res.data.message);
+			}
+		} catch (error) {
+			console.log(error);
+			message.error("something went wrong");
+		}
+	};
+	return (
+		<div className="form-container">
+			<Form
+				layout="vertical"
+				onFinish={onfinishHandler}
+				className="register-form p-5">
+				<h3 className="text-center">Login From</h3>
+
+				<Form.Item label="Email" name="email">
+					<Input type="email" required />
+				</Form.Item>
+				<Form.Item label="Password" name="password">
+					<Input type="password" required />
+				</Form.Item>
+				<Link to="/register" className="m-2">
+					Not a user Register here
+				</Link>
+				<div className="mt-4">
+					<button className="btn btn-primary" type="submit">
+						Login
+					</button>
+				</div>
+			</Form>
+		</div>
+	);
 };
 
 export default Login;
